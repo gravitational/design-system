5525e364bb5629a839473e0a32bbcf5c723ff863
Ryan Clark <ryan.clark@goteleport.com>
2025-11-13 18:53:06 +0100

    Remove pack from build

--- crates/swc-plugin-styled-components/build.sh
+++ crates/swc-plugin-styled-components/build.sh
@@ -9,4 +9,3 @@ cargo build --release -p swc_plugin_styled_components --target wasm32-wasip1
 cp ../../target/wasm32-wasip1/release/swc_plugin_styled_components.wasm dist
 cp package.json dist
 cp README.md dist
-cd dist && pnpm pack --out package.tgz
