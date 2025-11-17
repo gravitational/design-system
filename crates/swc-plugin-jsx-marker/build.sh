c5b206b842564bbc9191c996c145423708f06d31
Ryan Clark <ryan.clark@goteleport.com>
2025-11-17 11:12:40 +0100

    Add back publish config, remove it from dist/package.json instead

--- crates/swc-plugin-jsx-marker/build.sh
+++ crates/swc-plugin-jsx-marker/build.sh
@@ -7,5 +7,5 @@ mkdir -p dist
 
 cargo build --release -p swc_plugin_jsx_marker --target wasm32-wasip1
 cp ../../target/wasm32-wasip1/release/swc_plugin_jsx_marker.wasm dist
-cp package.json dist
+jq 'del(.publishConfig)' package.json > dist/package.json
 cd dist && pnpm pack --out package.tgz
