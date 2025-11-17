576889875888628da23cfdad63bf73eeab018eaf
Ryan Clark <ryan.clark@goteleport.com>
2025-11-17 14:49:18 +0100

    Remove publish config from SWC plugins distributed package.json (#73)
    * Remove publish config from SWC plugins
    
    * Update lockfile
    
    * Add back publish config, remove it from dist/package.json instead

--- crates/swc-plugin-styled-components/build.sh
+++ crates/swc-plugin-styled-components/build.sh
@@ -7,6 +7,6 @@ mkdir -p dist
 
 cargo build --release -p swc_plugin_styled_components --target wasm32-wasip1
 cp ../../target/wasm32-wasip1/release/swc_plugin_styled_components.wasm dist
-cp package.json dist
+jq 'del(.publishConfig)' package.json > dist/package.json
 cp README.md dist
 cd dist && pnpm pack --out package.tgz
